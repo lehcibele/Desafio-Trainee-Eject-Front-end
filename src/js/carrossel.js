99cb7f4c5187a7e57460e140ee65141c7edf0d2a
bowie <leticiacibele.cl@gmail.com>
2024-06-24 17:26:18 -0300

    atualização do carrossel

--- src/js/carrossel.js
+++ src/js/carrossel.js
@@ -1,31 +1,21 @@
-const control = document.querySelectorAll('.control');
-let currentItem = 0;
-const itens = document.querySelectorAll('.item');
-const maxItens = itens.length;
+$(document).ready(function(){
+    $('.carrossel').slick({
+        slidesToShow: 3,
+        slideToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        infinite: true,
 
-control.forEach(control => {
-    control.addEventListener('click', () => {
-        const esquerda = control.classList.contains('seta-esquerda');
-        
-        if(esquerda) {
-            currentItem -= 1;
-        } else {
-            currentItem += 1;
-        }
-
-        if(currentItem >= maxItens) {
-            currentItem = 0;
-        }
-
-        if(currentItem < 0) {
-            currentItem = maxItens - 1;
-        }
-        
-        itens.forEach(item => item.classList.remove('proximo-item'));
-
-        itens[currentItem].scrollIntoView({
-            inline: "center",
-            behavior: "smooth",
-        });
+        variableWidth: true,
+        prevArrow: '<img class="slick-prev" src="src/img/seta-esquerda.png" alt="">',
+        nextArrow: '<img class="slick-next" src="src/img/seta-direita.png" alt="">',
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+        ]
     })
-})
+});
