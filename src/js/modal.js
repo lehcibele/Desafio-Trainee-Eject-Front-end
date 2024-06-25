aff26b6c2eca065e29dc38e6142a948193828f10
bowie <leticiacibele.cl@gmail.com>
2024-06-24 23:57:29 -0300

    atualização modal js

--- src/js/modal.js
+++ src/js/modal.js
@@ -20,27 +20,3 @@ document.querySelectorAll('.close').forEach(button => {
         closeModal(modalId);
     });
 });
-
-
-/*
-function openModal() {
-    const modal = document.getElementById('modal1');
-    modal.classList.add('open');
-
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'modal1' || e.target.id == 'close') {
-            closeModal();
-            //modal.classList.remove('open');
-            //localStorage.fechaModal = 'modal-container'
-        }
-    });
-}
-
-function closeModal() {
-    const modal = document.getElementById('modal1');
-    modal.classList.remove('open');
-}
-
-document.getElementById('close').addEventListener('click', closeModal);
-
-*/
